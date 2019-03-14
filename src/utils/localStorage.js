05171743e46e4732cd8aedcd16cdd13eded545c1
Rener Baffa da Silva <renerbaffa@gmail.com>
2019-03-14 22:28:56 +0100

    Ignoring untestable code

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,15 +1,31 @@
 export function setTeamOnLocalStorage(team) {
-  window.localStorage.setItem('team', JSON.stringify(team))
+  /* istanbul ignore next: if is a workaround for gatsby to break during build */
+  if (typeof window !== `undefined`) {
+    window.localStorage.setItem('team', JSON.stringify(team))
+  }
 }
 
 export function getTeamFromLocalStorage() {
-  return JSON.parse(window.localStorage.getItem('team'))
+  /* istanbul ignore next: if is a workaround for gatsby to break during build */
+  if (typeof window !== `undefined`) {
+    return JSON.parse(window.localStorage.getItem('team'))
+  } else {
+    return { players: [] }
+  }
 }
 
 export function setGameOnLocalStorage(game) {
-  window.localStorage.setItem('game', JSON.stringify(game))
+  /* istanbul ignore next: if is a workaround for gatsby to break during build */
+  if (typeof window !== `undefined`) {
+    window.localStorage.setItem('game', JSON.stringify(game))
+  }
 }
 
 export function getGameFromLocalStorage() {
-  return JSON.parse(window.localStorage.getItem('game'))
+  /* istanbul ignore next: if is a workaround for gatsby to break during build */
+  if (typeof window !== `undefined`) {
+    return JSON.parse(window.localStorage.getItem('game'))
+  } else {
+    return {}
+  }
 }
